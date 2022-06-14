7f9c6d0f45b7b96c34513bcf2e8cb3727a5bff37
Angel Mitov <106675300+Mitoff@users.noreply.github.com>
2022-06-15 00:28:47 +0300

    Delete 02_multiples_list.py

--- 02_multiples_list.py
+++
@@ -1,8 +0,0 @@
-factor = int(input())
-count = int(input())
-
-list_of_numbers = []
-
-for multiplier in range(1, count + 1):
-    list_of_numbers.append(factor * multiplier)
-print(list_of_numbers)
